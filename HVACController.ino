12ee9e15106aab79f52d02adbf8ab6bf9f6482f3
Alex Roetter <aroetter@users.noreply.github.com>
2019-09-12 16:58:30 -0600

    First crack at actual A/B thermostat logic to decide ultimate result

--- HVACController.ino
+++ HVACController.ino
@@ -17,6 +17,14 @@
 #define HVACOUT_W1  9
 #define HVACOUT_Y1 10
 
+// the controls for the physical air ducts
+#define VENT_A 11
+#define VENT_B 12
+
+// i believe the relays are reversed, so switch these. for debugging now HIGH=open is nice.
+#define VENT_OPEN     HIGH
+#define VENT_CLOSED    LOW
+ 
 // runs once
 void setup() {
   Serial.begin(57600);
@@ -34,24 +42,120 @@ void setup() {
   pinMode(HVACOUT_W1, OUTPUT);
   pinMode(HVACOUT_Y1, OUTPUT);
 
-  Serial.println("Hello from setup...");
+  pinMode(VENT_A, OUTPUT);
+  pinMode(VENT_B, OUTPUT);
 }
 
 
+typedef struct NestState {
+  int G;
+  int W1;
+  int Y1;
+} NestState;
 
-// runs repeatedly
-void loop() {
-  // silly cycle through 3 output LEDs for now
-  Serial.println("Hello from loop()...");
+// silly cycle through 3 output LEDs for now
+// TODO: delete me
+void   debugCycle3OutputLEDs() {
   digitalWrite(HVACOUT_Y1, LOW);
   digitalWrite(HVACOUT_G, HIGH);
-  delay(1000 /* in ms */);
+  delay(300 /* in ms */);
 
   digitalWrite(HVACOUT_G, LOW);
   digitalWrite(HVACOUT_W1, HIGH);
-  delay(1000 /* in ms */);
+  delay(300);
 
   digitalWrite(HVACOUT_W1, LOW);
   digitalWrite(HVACOUT_Y1, HIGH);
-  delay(1000);
+  delay(300);
+
+}
+
+int isHeating(struct NestState nest) {
+  return nest.W1;
+}
+
+int isCooling(struct NestState nest) {
+  return nest.Y1;
+}
+
+int isBlowing(struct NestState nest) {
+  return nest.G;
+}
+
+void setHVACFromNest(NestState nest) {
+  digitalWrite(HVACOUT_G, nest.G);
+  digitalWrite(HVACOUT_W1, nest.W1);
+  digitalWrite(HVACOUT_Y1, nest.Y1);
+}
+
+void setVent(int vent, int setting) {
+  digitalWrite(vent, setting);
+}
+
+// runs repeatedly
+#define OUTBUF_LEN (80)
+void loop() {
+  NestState nesta, nestb;
+  nesta = {digitalRead(NESTA_G), digitalRead(NESTA_W1), digitalRead(NESTA_Y1)};
+  nestb = {digitalRead(NESTB_G), digitalRead(NESTB_W1), digitalRead(NESTB_Y1)};
+
+  char outbuf[OUTBUF_LEN];
+  snprintf(outbuf, OUTBUF_LEN, "Nest A: G:%d W1:%d Y1:%d.     Nest B: G:%d W1:%d Y1:%d",
+    nesta.G, nesta.W1, nesta.Y1, nestb.G, nestb.W1, nestb.Y1);
+  Serial.println(outbuf);
+
+
+  // both are heading
+  if ((isHeating(nesta) && isHeating(nestb))
+   || (isCooling(nesta) && isCooling(nestb))
+   || (isBlowing(nesta) && isBlowing(nestb))) {
+    setHVACFromNest(nesta);
+    setVent(VENT_A, VENT_OPEN);
+    setVent(VENT_B, VENT_OPEN);
+    // assert isBlowing(nestA) && isBlowing(nestB)
+
+    snprintf(outbuf, OUTBUF_LEN, "Case 1: Both thermostats are %s",
+      (isHeating(nesta) ? "heating" : (isCooling(nesta) ? "cooling" : "blowing")));
+    Serial.println(outbuf);
+  } else if (isHeating(nesta) || isCooling(nesta)) {
+    // if 2 thermostats are in different Heat/AC modes, "A" wins
+    // assert(isBlowing(nesta))
+    setHVACFromNest(nesta);
+    setVent(VENT_A, VENT_OPEN);
+    setVent(VENT_B, VENT_CLOSED);
+
+    snprintf(outbuf, OUTBUF_LEN, "Case 2: Thermostat A is %s, it wins.",
+      (isHeating(nesta) ? "heating" : "cooling"));
+    Serial.println(outbuf);
+  } else if (isHeating(nestb) || isCooling(nestb)) {
+    // B is commanding either heat or cool, so do room B only.
+    // assert(isBlowing(nestb)
+    setHVACFromNest(nestb);
+    setVent(VENT_A, VENT_CLOSED);
+    setVent(VENT_B, VENT_OPEN);
+    
+    snprintf(outbuf, OUTBUF_LEN, "Case 3: Thermostat B is %s, it wins.",
+      (isHeating(nestb) ? "heating" : "cooling"));
+    Serial.println(outbuf);
+  } else if (isBlowing(nesta)) {
+    setHVACFromNest(nesta);
+    setVent(VENT_A, VENT_OPEN);
+    setVent(VENT_B, VENT_CLOSED);
+
+    Serial.println("Case 4: Thermostat A is blowing, it wins.");
+  } else if (isBlowing(nestb)) {
+    setHVACFromNest(nestb);
+    setVent(VENT_A, VENT_CLOSED);
+    setVent(VENT_B, VENT_OPEN);
+
+    Serial.println("Case 5: Thermostat B is blowing, it wins.");
+  } else { // neither thermostat doing anything...
+    // assert everything is off;
+    setHVACFromNest(nesta);
+    setVent(VENT_A, VENT_OPEN);
+    setVent(VENT_B, VENT_OPEN);
+    Serial.println("Case 6: Neither thermostat doing anything.");
+  }
+
+  delay(1000);  
 }
